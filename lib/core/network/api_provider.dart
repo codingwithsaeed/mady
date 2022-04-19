afda96a0e8a9a71c2cd6088164b5949f218a920c
Saeed Ahmadi <codingwithsaeed@gmail.com>
2022-04-19 21:41:47 +0430

    added android icons

--- lib/core/network/api_provider.dart
+++ lib/core/network/api_provider.dart
@@ -4,10 +4,11 @@ import 'package:http/http.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mady/core/errors/exception.dart';
 
+// ignore: unused_element
 const _localUrl = 'http://192.168.1.2/mady/webservice.php';
 // ignore: unused_element
 const _onlineUrl = 'https://codingwithsaeed.ir/api/mady/webservice.php';
-final currentDataUrl = Uri.parse(_localUrl);
+final currentDataUrl = Uri.parse(_onlineUrl);
 
 abstract class ApiProvider {
   ///Performs a POST request to given url
